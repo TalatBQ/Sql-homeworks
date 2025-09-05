6091e9d9caccf1cf13ec7cfa1d2ea71b768519e0
TalatBQ <87007100+TalatBQ@users.noreply.github.com>
2025-09-05 20:51:24 +0500

    Create hw.sql

---
+++ lesson-1/homework/hw.sql
@@ -0,0 +1,49 @@
+Easy
+	1.
+		Data is a collection of information in the form of words or numbers.
+		Database is a place where data is stored.
+		Relational database is that where tables have relation with each other.
+		A table is a collection of related things.
+	3.
+		Windows Authentication and SQL Server Authentication
+Medium
+	
+	4. Create database SchoolDB
+
+	5. Create table Students (
+		StudentID INT PRIMARY KEY, 
+		Name VARCHAR(50), 
+		Age INT
+		);
+
+	6.
+		SQL (Structured Query Language) is programming language 
+		which is used to communicate with a database.
+
+		SQL Server is a software product of Microsoft.
+
+		SQL Server Management Studio (SSMS) is a interface tool which provides
+		a visual environment to create and modify database objects
+		
+Hard
+
+	7.
+		Data Query Language (DQL): Used to retrieve data from a database (Select)
+			
+		Data Manipulation Language (DML): Used to manage data within database objects.
+			(Insert into, Update, Delete from)
+		
+		Data Definition Language (DDL): Used to define and manage database objects.
+			(Create database/table, Alter table, Drop table)
+
+		Data Control Language (DCL): Used to manage database access and permissions.
+			(Grant, Revoke)
+
+		Transaction Control Language (TCL): Used to manage transactions within a database.
+			(Commit, Rollback)
+
+	8.
+		Insert into Students (StudentID, Name, Age) Values
+			(1, 'Jhon', 22),
+			(2, 'Alice', 19),
+			(3, 'Daniel', 21);
